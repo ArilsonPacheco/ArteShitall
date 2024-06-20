1c79893a6ac36a4c1da928c0b1735f342a3fab43
ArilsonPacheco <148496393+ArilsonPacheco@users.noreply.github.com>
2024-06-20 09:40:06 -0300

    Add files via upload

--- controllers/Grupos.py
+++ controllers/Grupos.py
@@ -6,6 +6,6 @@ def RecDF(rd):
     return pd.DataFrame.from_records(rd, columns=["Grupo", "Alunos"])
 
 def ListaGrupos():
-    qryGrupo = 'select "DS_Grupo", count("NM_Aluno") as qtdAluno from "Grupo_Aluno", "Grupo", "Aluno" where "Grupo"."IDGrupo" = "fk_Grupo_rGrupo" and "IDAluno" = "fk_Aluno_rAluno" group by "DS_Grupo"'
+    qryGrupo = 'select "DS_Grupo", count("NM_Aluno") as qtdAluno from "Grupo_Aluno", "Grupo", "Aluno" where "Grupo"."IDGrupo" = "fk_Grupo_rGrupo" and "IDAluno" = "fk_Aluno_rAluno" and "Ativo" = true group by "DS_Grupo"'
     db.curssor.execute(qryGrupo)
     return RecDF(db.curssor.fetchall())
